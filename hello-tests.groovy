407cdac5c75590b4762e7bbd423be34699a105c8
Michael J. Simons <michael@simons.ac>
2017-03-14 10:53:31 +0100

    Use real integration test

--- hello-tests.groovy
+++ hello-tests.groovy
@@ -1,9 +1,27 @@
+import org.junit.runner.RunWith
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = HelloWorldController.class)
+@WebMvcTest
 class HelloWorldControllerTest {
+    @Autowired
+    private MockMvc mockMvc
+    
     @Test
-    void helloWorldShouldWork() throws Exception {
-        assertEquals(
-            "Hello, Reader\n", 
-            new HelloWorldController()
-                .hello("Reader"))
+    void helloWorldShouldWork() throws Exception {        
+        this.mockMvc
+            .perform(get("/hello").param("name", "World"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Hello, World\n"))
     }
 }
